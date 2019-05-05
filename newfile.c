4d323c65342c2bf231f149c102549a92952f99a2
Rijo <rijomundassery@gmail.com>
2019-05-05 18:42:30 +0100

    Adding comment

--- newfile.c
+++ newfile.c
@@ -4,5 +4,5 @@ int main ()
 {
 	printf("This is to test github working\n");
 	printf("This is another test for checking the versions of the git\n");
-	//Adding a comment
+	//Adding a comment to test
 }
